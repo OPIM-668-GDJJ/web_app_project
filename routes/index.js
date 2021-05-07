859ec0f31deeac00b39c196bf277eb196d9bdf8e
JPMurtha3 <jpm392@georgetown.edu>
2021-05-06 20:29:22 -0500

    Updates to Recipe Route

--- routes/index.js
+++ routes/index.js
@@ -11,13 +11,9 @@ router.get('/about', function(req, res, next) {
   res.render('about');
 });
 
-/* GET hello page. */
-router.get('/hello', function(req, res, next) {
-  console.log("URL PARAMS:", req.query)
-  var name = req.query.name || "World" // double pipes is an OR operator that allows us to use a default value if the url params are null / not specified
-  var message = "Hello, " + name
-  res.render('hello', { message: message });
+/* GET recipes page. */
+router.get('/recipes', function(req, res, next) {
+  res.render('recipes');
 });
-
 
 module.exports = router;
